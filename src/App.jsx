d995a57b7fef9f95f7d9cca552d377ea713a93aa
kalbert contreras <kalbert.contreras@gmail.com>
2023-01-25 17:12:17 -0300

    se agrega drawer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import {ResponsiveAppBar} from "./components/navbar"
 import {IconButtonSizes} from "./components/bottons"
 import {Imagenes, StandardImageList} from "./components/imagen"
+import { SwipeableTemporaryDrawer } from "./components/drawe";
 function App() {
   return (
     <div>
         <ResponsiveAppBar></ResponsiveAppBar>
         <IconButtonSizes></IconButtonSizes>
+        <SwipeableTemporaryDrawer></SwipeableTemporaryDrawer>
         <Imagenes></Imagenes>
     </div>
+    
   );
 }
 
